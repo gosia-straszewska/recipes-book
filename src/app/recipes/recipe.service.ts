beab0706ab7f5ff71165c3130fc655579ef815cc
Straszewska Małgorzata <malgorzata.straszewska@finture.com>
2020-01-16 15:39:25 +0100

    EventEmmiter changed for Subject (shopping-list.component)

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,4 +1,4 @@
-import { EventEmitter, Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
 
 import { Recipe } from './recipe.model';
 import { Ingredient } from '../shared/ingredient.model';
@@ -6,13 +6,12 @@ import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
 @Injectable()
 export class RecipeService {
-    recipeSelected = new EventEmitter<Recipe>();
-    
+
     private recipes: Recipe[] = [
         new Recipe(
-            'Pizza', 
-            'Neapolitana', 
-            "https://media.fshoq.com/images/316/pizza-with-vegetables-on-a-wooden-table-316-medium.jpg",
+            'Pizza',
+            'Neapolitana',
+            'https://media.fshoq.com/images/316/pizza-with-vegetables-on-a-wooden-table-316-medium.jpg',
             [
                 new Ingredient ('Flour', 1),
                 new Ingredient ('Tomatoes', 10)
@@ -20,12 +19,12 @@ export class RecipeService {
         new Recipe(
             'Pesto Penne',
              'Delicious Pasta',
-              "https://p1.pxfuel.com/preview/328/443/588/pesto-pasta-spaghetti-parmesan.jpg",
+              'https://p1.pxfuel.com/preview/328/443/588/pesto-pasta-spaghetti-parmesan.jpg',
               [
                 new Ingredient ('Penne', 1),
                 new Ingredient ('Pesto', 1)
               ])
-    
+
       ];
 
     constructor(private shoppingListService: ShoppingListService){};
