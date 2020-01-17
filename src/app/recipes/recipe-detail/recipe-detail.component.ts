0aeae20e22207e1daab19a8bbfb54321cd7e6635
Straszewska Małgorzata <malgorzata.straszewska@finture.com>
2020-01-17 16:55:33 +0100

    forms

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -12,9 +12,9 @@ import { RecipeService } from '../recipe.service';
 export class RecipeDetailComponent implements OnInit {
   recipe: Recipe;
   id: number;
-  
-  
-  constructor(private recipeService:RecipeService,
+
+
+  constructor(private recipeService: RecipeService,
               private route: ActivatedRoute,
               private router: Router,
               ) { }
@@ -23,16 +23,21 @@ export class RecipeDetailComponent implements OnInit {
     this.route.params
     .subscribe((params: Params) => {this.id = +params['id'];
     this.recipe = this.recipeService.getRecipe(this.id);
-  })
+  });
   }
 
-  onAddToShoppingList(){
-    this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients)
+  onAddToShoppingList() {
+    this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);
   }
 
-  onEditRecipe(){
-    this.router.navigate(['edit'], {relativeTo: this.route})
+  onEditRecipe() {
+    this.router.navigate(['edit'], {relativeTo: this.route});
     // this.router.navigate(['../', this.id, 'edit'], {relativeTo: this.route})
+  }
+
+  onDeleteRecipe() {
+    this.recipeService.deleteRecipe(this.id);
+    this.router.navigate(['/recipes']);
   }
 
 }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -3,9 +3,11 @@ import { Injectable } from '@angular/core';
 import { Recipe } from './recipe.model';
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list/shopping-list.service';
+import { Subject } from 'rxjs';
 
 @Injectable()
 export class RecipeService {
+  recipesChanged = new Subject<Recipe[]>();
 
     private recipes: Recipe[] = [
         new Recipe(
@@ -27,17 +29,32 @@ export class RecipeService {
 
       ];
 
-    constructor(private shoppingListService: ShoppingListService){};
+    constructor(private shoppingListService: ShoppingListService) {}
 
     getRecipes() {
         return this.recipes.slice();
     }
 
-    getRecipe (index: number) {
+    getRecipe(index: number) {
         return this.recipes[index];
     }
 
-    addIngredientsToShoppingList(ingredients: Ingredient[]){
-        this.shoppingListService.addIngredients(ingredients)
+    addIngredientsToShoppingList(ingredients: Ingredient[]) {
+        this.shoppingListService.addIngredients(ingredients);
+    }
+
+    addRecipe(recipe: Recipe) {
+      this.recipes.push(recipe);
+      this.recipesChanged.next(this.recipes.slice());
+    }
+
+    updateRecipe(index: number, newRecipe: Recipe) {
+      this.recipes[index] = newRecipe;
+      this.recipesChanged.next(this.recipes.slice());
+    }
+
+    deleteRecipe(index: number) {
+      this.recipes.splice(index, 1);
+      this.recipesChanged.next(this.recipes.slice());
     }
 }
